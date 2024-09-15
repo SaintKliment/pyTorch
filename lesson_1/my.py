59c2a7dd841d840e58b9646668ce7c8399643208
SaintKliment <164558433+SaintKliment@users.noreply.github.com>
2024-09-15 19:35:59 +0300

    Creating a tensor with a specific size and default values

--- lesson_1/my.py
+++ lesson_1/my.py
@@ -2,3 +2,15 @@ import torch
 
 tensor_1 = torch.tensor([1.2, 2.3, 4.5])
 print(tensor_1)
+
+#create tensor zero
+tensor_zeros = torch.zeros(4, 5)
+print(tensor_zeros)
+
+# tensor one
+tensor_ones = torch.ones(2,3)
+print(tensor_ones)
+
+# tensor rand
+tensor_rand = torch.rand(4, 4)
+print(tensor_rand)
