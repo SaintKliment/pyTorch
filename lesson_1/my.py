90f47e8961eece838091c2433803fad7c852ea62
SaintKliment <164558433+SaintKliment@users.noreply.github.com>
2024-09-15 19:40:40 +0300

    Indexing and slicing, Creating a tensor with a specific data type

--- lesson_1/my.py
+++ lesson_1/my.py
@@ -1,16 +1,25 @@
 import torch
 
-tensor_1 = torch.tensor([1.2, 2.3, 4.5])
-print(tensor_1)
+# tensor_1 = torch.tensor([1.2, 2.3, 4.5])
+# print(tensor_1)
 
-#create tensor zero
-tensor_zeros = torch.zeros(4, 5)
-print(tensor_zeros)
+# #create tensor zero
+# tensor_zeros = torch.zeros(4, 5)
+# print(tensor_zeros)
 
-# tensor one
-tensor_ones = torch.ones(2,3)
-print(tensor_ones)
+# # tensor one
+# tensor_ones = torch.ones(2,3)
+# print(tensor_ones)
 
-# tensor rand
-tensor_rand = torch.rand(4, 4)
-print(tensor_rand)
+# # tensor rand
+# tensor_rand = torch.rand(4, 4)
+# print(tensor_rand)
+
+tensor_int = torch.tensor([1,2,3], dtype=torch.int32)
+print(tensor_int)
+
+tensor_slice = torch.tensor([[1,2,3], [4,5,6]])
+last_el = tensor_slice[1, 2] # 6
+last_el2 = tensor_slice[1, 1] # 5
+last_el3 = tensor_slice[0, 2] # 3
+print(last_el, last_el2, last_el3)
